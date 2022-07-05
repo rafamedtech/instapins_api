a4f73a2f3be395881adbcd3db5ade792c43db17f
Rafael <rafa.ravg@gmail.com>
2022-07-05 08:48:46 -0700

    code clean up

--- chat-backup/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import MessageAPIView
-
-
-urlpatterns = [
-    path('', MessageAPIView.as_view()),
-    path('<str:username>/', MessageAPIView.as_view()),
-]
