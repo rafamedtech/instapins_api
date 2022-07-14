3c469b32e1c7af40c16f15fd0bd0767ca3440774
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-07-14 12:35:53 -0700

    hosts test

--- api/settings.py
+++ api/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-$w--q#xr&kqbz3uj^&!4qw1$@m^_l7hug#_n=c=f$%d4&w79(h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['instapins.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [ 'http://localhost:3000', 'http://127.0.0.1:3000', 'instapins.herokuapp.com']
 
 
 # Application definition
