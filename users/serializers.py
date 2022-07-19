9315991b340c890bfb36a25625c0730b9e9a38dc
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-07-19 07:45:49 -0700

    avatar test

--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,7 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('id', 'email', 'username', 'password')
+        fields = ('id', 'email', 'username', 'password', 'avatar')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
