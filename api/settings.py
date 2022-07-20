c896f5b1d6efc8b9489b086edd56e0049aacf3e3
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-07-20 11:11:35 -0700

    client cors allowed

--- api/settings.py
+++ api/settings.py
@@ -114,7 +114,7 @@ CORS_ORIGIN_WHITELIST = (
 
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://instapins.netlify.app'
+    'https://instapins.herokuapp.com'
 )
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
